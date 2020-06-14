0da3b45df2952a1e43cbc647c853f09cbf2e4580
yurilamijo <yurilamijo@hotmail.com>
2020-06-14 12:19:34 +0200

    Add project files.

---
+++ DTT-Test/Models/Article.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DTT_Test.Models
+{
+    public partial class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int IsArchived { get; set; }
+    }
+}
---
+++ DTT-Test/Models/DTTContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DTT_Test.Models
+{
+    public class DTTContext : DbContext
+    {
+        public virtual DbSet<Article> Article { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=DTT;user=root;password=yuri_root;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Article>(entity =>
+            {
+                entity.ToTable("article");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11)")
+                    .IsRequired();
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasColumnName("description")
+                    .HasMaxLength(225)
+                    .IsUnicode(false)
+                    .IsRequired();
+
+                entity.Property(e => e.IsArchived)
+                    .HasColumnName("isArchived")
+                    .HasColumnType("int(1)")
+                    .IsRequired();
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasColumnName("title")
+                    .HasMaxLength(80)
+                    .IsUnicode(false)
+                    .IsRequired();
+            });
+
+        }
+    }
+}
